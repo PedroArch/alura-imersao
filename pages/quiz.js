06e4c04883ce7b040b686d1945092f4dd20cc7e4
Pedro Carvalho <pedrofrancocarvalho@gmail.com>
2021-01-28 22:36:49 -0300

    third commit day 3.1

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import db from '../db.json';
 
 import QuizBackground from '../src/components/QuizBackground';
@@ -9,38 +10,91 @@ import Button from '../src/components/Button';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Spinner from '../src/components/Spinner';
 
-function quiz() {
-  const screenStates = {
-    LOADING: 'LOADING',
-    QUIZ: 'QUIZ',
-    RESULTADO: 'RESULTADO',
-  };
+function QuestionWidget({
+  onSubmit,
+  questaoAtual,
+  totalQuestoes,
+  questaoIndex,
+}) {
+  return (
+    <Widget>
+      <Widget.Header>
+        { `Pergunta ${questaoIndex + 1} de ${totalQuestoes}` }
+      </Widget.Header>
+      <img
+        alt="Descrição"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={questaoAtual.image}
+      />
+      <Widget.Content>
+        <h2>{questaoAtual.title}</h2>
+        <p>{questaoAtual.description}</p>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            onSubmit();
+          }}
+        >
+          {questaoAtual.alternatives.map((alternativa, index) => (
+            <Widget.Topic
+              as="label"
+              htmlFor={`alternative__${index}`}
+              key={alternativa}
+            >
+              <input
+              // style={{ display: 'none' }}
+                id={`alternative__${index}`}
+                name={`questao__${questaoIndex}`}
+                type="radio"
+                key={alternativa}
+              />
+              {alternativa}
+            </Widget.Topic>
+          ))}
+          <Button type="submit">CONFIRMAR</Button>
+        </form>
+      </Widget.Content>
+    </Widget>
 
-  const [screenState, setScreenState] = React.useState(screenStates.QUIZ);
+  );
+}
+
+const screenStates = {
+  LOADING: 'LOADING',
+  QUIZ: 'QUIZ',
+  RESULTADO: 'RESULTADO',
+};
+
+export default function Quiz() {
+  const [screenState, setScreenState] = React.useState(screenStates.LOADING);
   const [acertos, setAcertos] = React.useState(0);
-  const [totalQuestoes, setTotalQuestoes] = React.useState(0);
+  const totalQuestoes = db.questions.length;
+  const [questaoIndex, setQuestaoIndex] = React.useState(0);
+  const questaoAtual = db.questions[questaoIndex];
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  function handleSubmit() {
+    const proximaQuestao = questaoIndex + 1;
+    if (proximaQuestao < totalQuestoes) {
+      setQuestaoIndex(proximaQuestao);
+    } else {
+      setScreenState(screenStates.RESULTADO);
+    }
   }
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1000);
+  }, []);
 
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <QuizLogo />
-        {screenState === screenStates.QUIZ && (
-          <Widget>
-            <Widget.Header>
-              <h1>{db.title}</h1>
-            </Widget.Header>
-            <Widget.Content>
-              <p>{db.description}</p>
-              <form onSubmit={handleSubmit}>
-                <Button type="submit">CONFIRMAR</Button>
-              </form>
-            </Widget.Content>
-          </Widget>
-        )}
         {screenState === screenStates.LOADING && (
           <Widget>
             <Widget.Header>
@@ -50,6 +104,15 @@ function quiz() {
               <Spinner />
             </Widget.Content>
           </Widget>
+        )}
+
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            onSubmit={handleSubmit}
+            questaoAtual={questaoAtual}
+            questaoIndex={questaoIndex}
+            totalQuestoes={totalQuestoes}
+          />
         )}
 
         {screenState === screenStates.RESULTADO && (
@@ -68,4 +131,15 @@ function quiz() {
   );
 }
 
-export default quiz;
+QuestionWidget.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  totalQuestoes: PropTypes.number.isRequired,
+  questaoIndex: PropTypes.number.isRequired,
+  questaoAtual: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    answer: PropTypes.number.isRequired,
+    alternatives: PropTypes.arrayOf(PropTypes.string.isRequired),
+  }).isRequired,
+};
