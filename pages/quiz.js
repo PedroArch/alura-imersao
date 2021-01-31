f9d2e3d9d135a2a74b0829df4da6162199a1be60
Pedro Carvalho <pedrofrancocarvalho@gmail.com>
2021-01-31 02:15:47 -0300

    fourth commit day 4"

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,3 +1,5 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import PropTypes from 'prop-types';
 import db from '../db.json';
@@ -9,17 +11,25 @@ import Widget from '../src/components/Widget';
 import Button from '../src/components/Button';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Spinner from '../src/components/Spinner';
+import AltertiveForm from '../src/components/AlternativeForm';
 
 function QuestionWidget({
   onSubmit,
   questaoAtual,
   totalQuestoes,
   questaoIndex,
+  setAlternativaSelecionada,
+  isSubmitted,
+  isCorrect,
+  disableButton,
+  setDisableButton,
+  alternativeStatus,
+  alternativaSelecionada,
 }) {
   return (
     <Widget>
       <Widget.Header>
-        { `Pergunta ${questaoIndex + 1} de ${totalQuestoes}` }
+        {`Pergunta ${questaoIndex + 1} de ${totalQuestoes}`}
       </Widget.Header>
       <img
         alt="Descrição"
@@ -33,33 +43,45 @@ function QuestionWidget({
       <Widget.Content>
         <h2>{questaoAtual.title}</h2>
         <p>{questaoAtual.description}</p>
-        <form
+        <AltertiveForm
           onSubmit={(event) => {
             event.preventDefault();
             onSubmit();
           }}
         >
-          {questaoAtual.alternatives.map((alternativa, index) => (
-            <Widget.Topic
-              as="label"
-              htmlFor={`alternative__${index}`}
-              key={alternativa}
-            >
-              <input
-              // style={{ display: 'none' }}
-                id={`alternative__${index}`}
-                name={`questao__${questaoIndex}`}
-                type="radio"
+          {questaoAtual.alternatives.map((alternativa, index) => {
+            const isSelected = alternativaSelecionada === index;
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={`alternative__${index}`}
                 key={alternativa}
-              />
-              {alternativa}
-            </Widget.Topic>
-          ))}
-          <Button type="submit">CONFIRMAR</Button>
-        </form>
+                data-selected={isSelected}
+                data-status={isSubmitted && alternativeStatus}
+              >
+                <input
+                  style={{ display: 'none' }}
+                  id={`alternative__${index}`}
+                  name={`questao__${questaoIndex}`}
+                  type="radio"
+                  key={alternativa}
+                  onChange={() => {
+                    setAlternativaSelecionada(index);
+                    setDisableButton(false);
+                  }}
+                />
+                {alternativa}
+              </Widget.Topic>
+            );
+          })}
+          <Button type="submit" disabled={disableButton}>
+            CONFIRMAR
+          </Button>
+          {isSubmitted && isCorrect && <p>ACERTOU!!!</p>}
+          {isSubmitted && !isCorrect && <p>ERROU!!!</p>}
+        </AltertiveForm>
       </Widget.Content>
     </Widget>
-
   );
 }
 
@@ -71,16 +93,36 @@ const screenStates = {
 
 export default function Quiz() {
   const [screenState, setScreenState] = React.useState(screenStates.LOADING);
-  const [acertos, setAcertos] = React.useState(0);
+  const [acertos, setAcertos] = React.useState([]);
   const totalQuestoes = db.questions.length;
   const [questaoIndex, setQuestaoIndex] = React.useState(0);
   const questaoAtual = db.questions[questaoIndex];
+  const [alternativaSelecionada, setAlternativaSelecionada] = React.useState(
+    null
+  );
+  const [isSubmitted, setIsSubmitted] = React.useState(false);
+  const isCorrect = alternativaSelecionada === questaoAtual.answer;
+  const [disableButton, setDisableButton] = React.useState(true);
+  const alternativeStatus = isCorrect ? 'SUCCESS' : 'ERROR';
+
+  function addResult() {
+    const resultado = isCorrect;
+    setAcertos([...acertos, resultado]);
+  }
 
   function handleSubmit() {
     const proximaQuestao = questaoIndex + 1;
     if (proximaQuestao < totalQuestoes) {
-      setQuestaoIndex(proximaQuestao);
+      setDisableButton(true);
+      setIsSubmitted(true);
+      setTimeout(() => {
+        setQuestaoIndex(proximaQuestao);
+        setIsSubmitted(false);
+        setAlternativaSelecionada(null);
+      }, 3 * 1000);
+      addResult();
     } else {
+      addResult();
       setScreenState(screenStates.RESULTADO);
     }
   }
@@ -112,6 +154,13 @@ export default function Quiz() {
             questaoAtual={questaoAtual}
             questaoIndex={questaoIndex}
             totalQuestoes={totalQuestoes}
+            alternativaSelecionada={alternativaSelecionada}
+            setAlternativaSelecionada={setAlternativaSelecionada}
+            isCorrect={isCorrect}
+            isSubmitted={isSubmitted}
+            disableButton={disableButton}
+            setDisableButton={setDisableButton}
+            alternativeStatus={alternativeStatus}
           />
         )}
 
@@ -120,8 +169,18 @@ export default function Quiz() {
             <Widget.Header>
               <h1>RESULTADO</h1>
             </Widget.Header>
-            <Widget.Content>
-              {`Você acertou ${acertos} de ${totalQuestoes} perguntas! PARABÉNS`}
+            <Widget.Content acertos={acertos}>
+              {`Você acertou ${
+                acertos.filter((x) => x).length
+              } de ${totalQuestoes} perguntas! PARABÉNS`}
+              <ul>
+                {acertos.map((acerto, index) => (
+                  <li key={`resutl__${index + 1}`}>
+                    #{index + 1} Questão:
+                    {acerto === true ? 'Acertou' : 'Errou'}
+                  </li>
+                ))}
+              </ul>
             </Widget.Content>
           </Widget>
         )}
@@ -132,7 +191,13 @@ export default function Quiz() {
 }
 
 QuestionWidget.propTypes = {
+  alternativeStatus: PropTypes.string.isRequired,
   onSubmit: PropTypes.func.isRequired,
+  isCorrect: PropTypes.bool.isRequired,
+  disableButton: PropTypes.bool.isRequired,
+  isSubmitted: PropTypes.bool.isRequired,
+  setAlternativaSelecionada: PropTypes.func.isRequired,
+  setDisableButton: PropTypes.func.isRequired,
   totalQuestoes: PropTypes.number.isRequired,
   questaoIndex: PropTypes.number.isRequired,
   questaoAtual: PropTypes.shape({
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -58,12 +58,30 @@ Widget.Topic = styled.a`
   margin-bottom: 8px;
   cursor: pointer;
   border-radius: ${({ theme }) => theme.borderRadius};
-  transition: .3s;
+  transition: 0.3s;
   display: block;
-  
+
   &:hover,
   &:focus {
-    opacity: .5;
+    opacity: 0.5;
+  }
+`;
+
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => `${theme.colors.primary}40`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: 0.3s;
+  display: block;
+
+  &:hover,
+  &:focus {
+    opacity: 0.5;
   }
 `;
 
